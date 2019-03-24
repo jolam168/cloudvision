2b6bfaeda9f4b67de1e19d30c0c47de9b1982ae5
Jo Lam <tsimehcppa@gmail.com>
2019-03-24 16:46:46 +0900

    add mlmodel

--- cloud.vision/ViewController.swift
+++ cloud.vision/ViewController.swift
@@ -3,19 +3,28 @@
 //  cloud.vision
 //
 //
-
+import CoreML
 import UIKit
 
+@available(iOS 11.0, *)
 class ViewController: UIViewController ,UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
     var imagePicker = UIImagePickerController()
-    
+	var model : ImageClassifier!
+	
     @IBOutlet weak var imagePicked: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
+	
+	override func viewWillAppear(_ animated: Bool) {
+		model = ImageClassifier()
+
+		
+	}
 
     @IBAction func buttonPressed(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
@@ -31,9 +40,34 @@ class ViewController: UIViewController ,UINavigationControllerDelegate, UIImageP
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
-        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+		guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+			return
+		}
+
+		
         imagePicked.image = image
         dismiss(animated:true, completion: nil)
+		
+		UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+		image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+		let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+		UIGraphicsEndImageContext()
+		
+		let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+		var pixelBuffer : CVPixelBuffer?
+		let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+		guard (status == kCVReturnSuccess) else {
+			return
+		}
+	
+		
+		guard let prediction = try? model.prediction(image: pixelBuffer!) else {
+			return
+		}
+		
+		print(prediction.classLabelProbs)
+//		classifier.text = "I think this is a \(prediction.classLabel)."
+		
     }
 }
 
